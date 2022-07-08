52b031b75ca29fcb812d7d832e3de0489bcaa298
HectorV2 <ganiriwand@gmail.com>
2022-07-08 15:51:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -25,14 +25,21 @@ const GoStumble = (auth) => new Promise((resolve, reject) => {
 
 (async () => {
 
-  console.log(chalk.greenBright(`
+  console.log(chalk.greenBright(chalk.bold`
 ██╗ ██████╗  █████╗ ███╗   ██╗██╗
 ██║██╔════╝ ██╔══██╗████╗  ██║██║
 ██║██║  ███╗███████║██╔██╗ ██║██║
 ██║██║   ██║██╔══██║██║╚██╗██║██║
 ██║╚██████╔╝██║  ██║██║ ╚████║██║
 ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝
-`));
+1. ${chalk.greenBright('Push Crown`}
+2. ${chalk.greenBright('Push Trophy')}
+3. ${chalk.greenBright('Push Trophy')}
+`)));
+
+ const feature = rs.question('[+] Enter feature needed : ');
+
+ if (feature == '1') {
 
   const auth = rs.question(chalk.greenBright('Enter Auth Keys : '));
   const time = rs.question(chalk.greenBright('Enter Delay In Milisecond : '));
@@ -40,6 +47,7 @@ const GoStumble = (auth) => new Promise((resolve, reject) => {
 
   while (true) {
 
+    var code = '3';
     const result = await GoStumble(auth);
     if (!result) {
 
@@ -63,9 +71,97 @@ const GoStumble = (auth) => new Promise((resolve, reject) => {
       await delay(time)
 
     } else if (result == 'BANNED') {
-      console.log(chalk.bgRed(`Your Account Has Been Banned`));
-      break;
-    }
+
+        console.log(chalk.red(`[ ${moment().format('HH:mm:ss')} ] Banned !`));
+        break;
+                
+     } else if (result == 'SERVER_ERROR' || result.includes('User path limit exceeded')) {
+
+         continue;
+                
+      }
+  }
+
+} else if (feature == '2') {
+
+  const auth = rs.question(chalk.greenBright('Enter Auth Keys : '));
+  const time = rs.question(chalk.greenBright('Enter Delay In Milisecond : '));
+  console.log('');
+
+  while (true) {
+
+    var code = '2';
+    const result = await GoStumble(auth);
+    if (!result) {
+
+      console.log(chalk.redBright(`Auth Sudah Expired`));
+
+    } else if (result.includes('User')) {
+
+      const data = JSON.parse(result);
+      const username = data.User.Username;
+      const country = data.User.Country;
+      const trophy = data.User.SkillRating;
+
+      console.log(chalk.greenBright(`\r
+♨  [${moment().format('HH:mm:ss')}]  ♨
+~  ${(`Country : ${country}`)}
+~  ${(`Username : ${username}`)}
+~  ${(`Trophy : ${trophy}`)}
+~  ${(`Status : ✓ Success`)}`));
+      await delay(time)
+
+    } else if (result == 'BANNED') {
+
+        console.log(chalk.red(`[ ${moment().format('HH:mm:ss')} ] Banned !`));
+        break;
+                
+     } else if (result == 'SERVER_ERROR' || result.includes('User path limit exceeded')) {
+
+         continue;
+                
+      }
+  }
+
+ } else if (feature == '3') {
+
+  const auth = rs.question(chalk.greenBright('Enter Auth Keys : '));
+  const time = rs.question(chalk.greenBright('Enter Delay In Milisecond : '));
+  console.log('');
+
+  while (true) {
+
+    var code = '1';
+    const result = await GoStumble(auth);
+    if (!result) {
+
+      console.log(chalk.redBright(`Auth Sudah Expired`));
+
+    } else if (result.includes('User')) {
+
+      const data = JSON.parse(result);
+      const username = data.User.Username;
+      const country = data.User.Country;
+      const trophy = data.User.SkillRating;
+
+      console.log(chalk.greenBright(`\r
+♨  [${moment().format('HH:mm:ss')}]  ♨
+~  ${(`Country : ${country}`)}
+~  ${(`Username : ${username}`)}
+~  ${(`Trophy : ${trophy}`)}
+~  ${(`Status : ✓ Success`)}`));
+      await delay(time)
+
+    } else if (result == 'BANNED') {
+
+        console.log(chalk.red(`[ ${moment().format('HH:mm:ss')} ] Banned !`));
+        break;
+                
+     } else if (result == 'SERVER_ERROR' || result.includes('User path limit exceeded')) {
+
+         continue;
+                
+      }
   }
 
 })();
